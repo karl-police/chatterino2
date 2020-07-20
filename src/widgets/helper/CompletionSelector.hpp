d5a5b8e5360991f9daeda7734cf6398889e4d97a
karl-police <karl-police2001@bluewin.ch>
2020-07-20 17:56:04 +0200

    Update CompletionSelector.hpp

--- src/widgets/helper/CompletionSelector.hpp
+++ src/widgets/helper/CompletionSelector.hpp
@@ -25,21 +25,7 @@ public:
 
     pajlada::Signals::Signal<Link> linkClicked;
 
-    void setCompleter(QCompleter *c);
-    QCompleter *completer() const;
-
-protected:
-    void keyPressEvent(QKeyEvent *e) override;
-    void focusInEvent(QFocusEvent *e) override;
-
-private slots:
-    void insertCompletion(const QString &completion);
-
-private:
-    QString textUnderCursor() const;
-
-private:
-    QCompleter *c = nullptr;
+    void matchList(const QString matchString);
 
 private:
     ChannelView *globalEmotesView_{};
