c78ce68fb235a559e1fab2fbdb6c97bfdec378c2
karl-police <karl-police2001@bluewin.ch>
2020-07-20 17:32:58 +0200

    Update CompletionSelector.hpp

--- src/widgets/helper/CompletionSelector.hpp
+++ src/widgets/helper/CompletionSelector.hpp
@@ -1,1 +1,47 @@
+#pragma once
 
+#include "widgets/BaseWindow.hpp"
+
+#include <pajlada/signals/signal.hpp>
+
+namespace chatterino {
+
+struct Link;
+class ChannelView;
+class Channel;
+using ChannelPtr = std::shared_ptr<Channel>;
+
+class CompletionSelector : public BaseWindow
+{
+public:
+    CompletionSelector(QWidget *parent = nullptr);
+
+    void loadChannel(ChannelPtr channel);
+    void loadEmojis();
+
+    virtual void closeEvent(QCloseEvent *event) override;
+
+    pajlada::Signals::Signal<Link> linkClicked;
+
+    void setCompleter(QCompleter *c);
+    QCompleter *completer() const;
+
+protected:
+    void keyPressEvent(QKeyEvent *e) override;
+    void focusInEvent(QFocusEvent *e) override;
+
+private slots:
+    void insertCompletion(const QString &completion);
+
+private:
+    QString textUnderCursor() const;
+
+private:
+    QCompleter *c = nullptr;
+
+private:
+    ChannelView *globalEmotesView_{};
+    ChannelView *channelEmotesView_{};
+    ChannelView *subEmotesView_{};
+    ChannelView *viewEmojis_{};
+};
