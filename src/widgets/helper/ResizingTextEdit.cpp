f03370f7fd6ba076ca1ff33e76539a8c15865166
karl-police <karl-police2001@bluewin.ch>
2020-07-25 14:56:35 +0200

    Update ResizingTextEdit.cpp

--- src/widgets/helper/ResizingTextEdit.cpp
+++ src/widgets/helper/ResizingTextEdit.cpp
@@ -142,7 +142,6 @@ void ResizingTextEdit::keyPressEvent(QKeyEvent *event)
                 //this->emoteSelector_->loadChannel(this->split_->getChannel());
                 this->emoteSelector_->show();
             };
-            this->emoteSelector_->refresh(currentCompletionPrefix);
             this->completionInProgress_ = true;
             this->completer_->setCompletionPrefix(currentCompletionPrefix);
             this->completer_->complete();
