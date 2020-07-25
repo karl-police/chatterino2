82c6103f5204c894b611747d09b604c347cc2dd9
karl-police <karl-police2001@bluewin.ch>
2020-07-25 15:15:54 +0200

    Update CompletionModel.cpp

--- src/common/CompletionModel.cpp
+++ src/common/CompletionModel.cpp
@@ -10,6 +10,7 @@
 #include "providers/twitch/TwitchIrcServer.hpp"
 #include "singletons/Emotes.hpp"
 #include "singletons/Settings.hpp"
+#include "widgets/helper/CompletionSelector.hpp"
 
 #include <QtAlgorithms>
 #include <utility>
